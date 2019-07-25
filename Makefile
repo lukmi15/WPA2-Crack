d8a23993d034503900873e068df69332a39d5b94
lukmi15 <lukas.mirow@web.de>
2019-07-25 11:59:29 +0200

    Added multicore support and some optimizations

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 #run: test.out Makefile
 #	./$<
 
-test: test.cpp wpa2_hash_wrapper.hpp wpa2_hash_wrapper.cpp sha1.c sha1.h Makefile wpa2_hash.h wpa2_hash.c
-	g++ --std=c++11 -Wall -o $@.out $< wpa2_hash.c sha1.c wpa2_hash_wrapper.cpp
+test: main.cpp wpa2_hash_wrapper.hpp wpa2_hash_wrapper.cpp sha1.c sha1.h Makefile wpa2_hash.h wpa2_hash.c
+	g++ --std=c++11 -Wall -o $@.out $< wpa2_hash.c sha1.c wpa2_hash_wrapper.cpp -fopenmp -O3 -march=native
 
 .PHONY: run
--- main.cpp
+++ main.cpp
@@ -1,18 +1,21 @@
-/*First test to calculate the hash of a WPA2 network password
+/*Calculating WPA2 PSK hashes
 Author(s)		: Lukas Mirow, contributors of the wpa_supplicant project
 */
 
 #include <iostream>
 #include "wpa2_hash_wrapper.hpp"
+#include <sstream>
+#include <omp.h>
 using namespace std;
 
 int main(int argc, char **argv)
 {
-	if (argc < 3)
+#pragma omp parallel for
+	for (size_t i = 0; i < 1800; i++)
 	{
-		printf("Too few arguments!\n");
-		exit(1);
+		stringstream ss;
+		ss << i;
+		calc_wpa2_hash("ssid", ss.str().c_str());
 	}
-	cout << hex << calc_wpa2_hash(argv[1], argv[2]) << endl;
 	return 0;
 }
