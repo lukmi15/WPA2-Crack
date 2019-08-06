65dbf48fd78b68840a822076d3313b44d7a03f84
Marvin Schneider <s0559506@htw-berlin.de>
2019-08-06 19:18:06 +0200

    Änderungsvorschläge umgesetzt.

--- benchmark.c
+++ benchmark.c
@@ -5,49 +5,58 @@
 #include <unistd.h>
 #include <time.h>
 
+#define HASHES 1800
+
+int runs;
+long unsigned delta_us, total_us, HperS, avgHperS;
+struct timespec start, end;
+
+void calcHperS()
+{
+	delta_us = (end.tv_sec - start.tv_sec) * 1000000 + (end.tv_nsec - start.tv_nsec) / 1000;
+        total_us += delta_us;
+        HperS = HASHES*1000000/delta_us;
+        printf("\tThe speed was about %li H/s.\n\n", HperS);
+}
+
+void conclusionHperS()
+{
+	avgHperS =HASHES*1000000/(total_us/runs);
+        printf("\nThe average Hashes per Seconds are %li.\n", avgHperS);
+        printf("For %i runs the programm needed %lu milliseconds.\n", runs, total_us/1000);
+}
 
 int main(int argc, char **argv)
 {
-	int runs, id, wait_param;
-	struct timespec start, end;
-	long unsigned delta_us, total_us, HperS;
+	int id, wait_param;
+	//struct timespec start, end;
 	char test[]="./test.out";
 
 	if(argc!=2)
-	{
-		printf("No arguments given!\n");
-		return 1;
-	}
+        {
+                printf("Too few parameters!\n");
+                return 1;
+        }
 
 	wait_param = 0;
-	total_us = 0;
 	runs = atoi(argv[1]);
+
 	for(int i=0; i<runs; i++)
 	{
 		clock_gettime(CLOCK_MONOTONIC_RAW, &start);
-
-		//do stuff
 		id = fork();
 		if (id == -1)
 			exit(1);
 		if (id == 0)
 		{
-			printf("child is now executing\n");
-			//execl("/usr/bin/mpirun", "mpirun", "--use-hwthread-cpus", "test.out", NULL);
-			execl(test, test, (char *)0);
+			printf("WPA2 Wrapper is now executing...\n");
+			execl(test, test, NULL);
 			exit(1);
 		}
-		printf("waiting for children\n");
 		wait(&wait_param);
 		clock_gettime(CLOCK_MONOTONIC_RAW, &end);
-		delta_us = (end.tv_sec - start.tv_sec) * 1000000 + (end.tv_nsec - start.tv_nsec) / 1000;
-		total_us += delta_us;
-		printf("It took %lu microseconds\n", delta_us);
+		calcHperS();
 	}
-	HperS =/*xxx Hashes /*/total_us/runs;
-	printf("Success, yay! \\(\")/\n");
-	printf("The average time for xxx Hashes was %li microseconds\n", HperS);
-	//das folgende printf ist nur zum Vergleich zum time Aufruf gedacht
-	printf("Comparision between total_us and the time argument: %lu\n", total_us);
+	conclusionHperS();
 	return 0;
 }
